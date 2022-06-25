320ed28d4273482a18833609a8e14d244398957c
Ker Tenepere <ker10epere@gmail.com>
2022-06-25 17:58:06 +0800

    add jsdoc to MyExpress interface

--- src/commons/interfaces/express.interface.ts
+++ src/commons/interfaces/express.interface.ts
@@ -1,12 +1,20 @@
 import { Request } from 'express'
-import { LoginDTO } from '../../dtos/login.dto'
 import { User } from '../../models/user.entity'
 import { MyDataSource } from './my-datasource.interface'
 
+/**
+ * @author Ker Tenepere
+ * @property {User} user  - to get this you will need to pass 2 middlewares in order
+ *  1. dataSourceProvider
+ *  2. auth
+ *
+ * @property {MyDataSource} dataSource  - this provides dataSource property to Request. Use this for type casting.
+ *
+ * this will only work if you initialized in middleware the dataSourceProvider middleware
+ */
+
 export interface MyRequest extends Request {
   user: User
-
-  body: LoginDTO
 
   dataSource: MyDataSource
 }
